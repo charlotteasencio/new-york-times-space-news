9ce97758698343a8128e52277bd2615d5ce4e57e
Charlotte Ennis <charlotteennis@Charlottes-MacBook-Pro.local>
2025-08-20 10:45:23 -0400

    add env

--- src/NewsArticlesGrid/NewsArticlesGrid.tsx
+++ src/NewsArticlesGrid/NewsArticlesGrid.tsx
@@ -13,13 +13,15 @@ export default function NewsArticlesGrid() {
     const [page, setPage] = useState(1)
     const targetRef = useRef<HTMLDivElement>(null);
 
+    const apiKey = process.env.REACT_APP_NYT_API_KEY;
+
     useEffect(() => {
         const fetchArticles = async () => {
             setLoading(true);
             try {
                 // articles are being fetched from the Space Flight news API: https://www.spaceflightnewsapi.net/
                 // the first 10 articles are fetched and the remaining articles are fetched on scroll
-                const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=space&page=1&api-key=jXZ0xRtYFkpoqJGuaf1vzBamjMh5ZW4B`);
+                const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=space&page=1&api-key=${apiKey}`);
                 const data = await response.json();
                 setArticles(data.response.docs);
                 setPage((prev) => prev + 1)
@@ -30,13 +32,13 @@ export default function NewsArticlesGrid() {
             }
         };
         fetchArticles();
-    }, []);
+    }, [apiKey]);
 
     //if this fetch pattern is going to be used in other places, we could extract into the custom hook
     const fetchMoreArticles = useCallback(async () => {
         setLoadingMore(true);
         try {
-            const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=space&page=${page}&api-key=jXZ0xRtYFkpoqJGuaf1vzBamjMh5ZW4B`);
+            const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=space&page=${page}&api-key=${apiKey}`);
             const data = await response.json();
             setArticles((prevArticles) => [...prevArticles, ...data.response.docs]);
             setPage((prev) => prev + 1)
@@ -45,7 +47,7 @@ export default function NewsArticlesGrid() {
         } finally {
             setLoadingMore(false);
         }
-    }, [page]);
+    }, [page, apiKey]);
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
